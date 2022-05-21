deceed0baec53ac696b8eaeae37b00d8ccb14867
Abhi <shineabhigupta1223@gmail.com>
2022-05-21 15:57:00 +0530

    switch file add in my reapo.

---
+++ switch/switch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    switch(x)
+	    {
+	        case 1:
+	            cout<<"one"<<"\n";
+	            break;
+	        case 2:
+	            cout<<"two"<<"\n";
+	            break;
+	        case 3:
+	            cout<<"three"<<"\n";
+	            break;
+	       
+	        
+	        default:
+	         cout<<"not in range"<<"\n";
+	    }
+	}
+	return 0;
+}
